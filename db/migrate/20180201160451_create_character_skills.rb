216c5395aed4dea79977799af1758be4e4f5d7e5
Joe Ziemba <jziemba90@gmail.com>
2018-02-01 11:09:24 -0500

    removed ability scores

--- db/migrate/20180201160451_create_character_skills.rb
+++ db/migrate/20180201160451_create_character_skills.rb
@@ -1,7 +1,11 @@
 class CreateCharacterSkills < ActiveRecord::Migration[5.1]
   def change
     create_table :character_skills do |t|
+      t.references  :characters
+      t.references  :skills
 
+      t.integer     :ranks
+      t.
       t.timestamps
     end
   end
--- spec/factories/scores.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :score do
-    
-  end
-end
